25e4e2c5b7c886ec4e537c170e4ac1c54d26252c
OBB-06 <olayabaabidane@gmail.com>
2024-12-16 22:17:23 +0100

    Inicializamos
    Inicializamos el random y el scanner

--- src/ejercicio/Matrices_Olaya.java
+++ src/ejercicio/Matrices_Olaya.java
@@ -1,5 +1,8 @@
 package ejercicio;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Matrices_Olaya {
 
 	public static void main(String[] args) {
@@ -7,7 +10,16 @@ public class Matrices_Olaya {
 
 		int[][] matriz = new int[5][5]; // Matriz de 5x5
 		
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
 		
+		System.out.println("Matriz:");
+		for (int i = 0; i < matriz.length; i++) { // Iterar filas
+		    for (int j = 0; j < matriz[i].length; j++) { // Iterar columnas
+		        System.out.print(matriz[i][j] + "-"); // Mostrar elemento
+		    }
+		    System.out.println(); // Salto de línea al final de cada fila
+		}
 		
 	}	
 
